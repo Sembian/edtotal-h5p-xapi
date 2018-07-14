40b84812c6c78753a27e66659fa30b3171feff04
sembianbalasubramanian <sembianb@gmail.com>
2018-07-14 14:30:06 -0500

    Initial Shell

---
+++ edtotal-h5p-xapi.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * H5P Plugin.
+ *
+ * Eases the tracking of rich interactive content
+ * into you blog. Find more info at http://edchunk.com
+ *
+ * @package   edtotal-h5p-xapi
+ * @author    Joubel <contact@edchunk.com>
+ * @license   MIT
+ * @link      http://edchunk.com
+ * @copyright 2018 edtotal.com
+ *
+ * @wordpress-plugin
+ * Plugin Name:       EDTOTAL H5P XPI
+ * Plugin URI:        http://edchunk.com/sandbox
+ * Description:       Allows you to track rich interactive content on your WordPress site.
+ * Version:           1.0.0
+ * Author:            Adam Spindell
+ * Author URI:        http://edchunk.com
+ * Text Domain:       edtotal h5P XPI
+ * License:           MIT
+ * License URI:       http://opensource.org/licenses/MIT
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/Sembian/edtotal-h5p-xpi.git
+ */
+
+// If this file is called directly, abort.
